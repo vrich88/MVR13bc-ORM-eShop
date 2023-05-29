ad72140f04d714257cfae00876c24a3800c327fa
vrich88 <mvrichards@icloud.com>
2023-05-29 16:30:09 -0500

    working up models

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,22 @@
+// set up dependencies
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
+// set up category params
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
